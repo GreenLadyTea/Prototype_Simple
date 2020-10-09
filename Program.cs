6813616a2a6e2ce792cb77d967388109b975bb8c
GreenLadyTea <72006003+GreenLadyTea@users.noreply.github.com>
2020-10-09 10:38:29 +0700

    Program
    program has been added

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    abstract class Prototype
+    {
+        public abstract Prototype Clone();
+    }
+
+    class ConcretePrototype1 : Prototype
+    {
+        public ConcretePrototype1()
+        {
+            Console.WriteLine("Prototype 1");
+        }
+        public override Prototype Clone()
+        {
+            return new ConcretePrototype1();
+        }
+    }
+
+    class ConcretePrototype2 : Prototype
+    {
+        public ConcretePrototype2()
+        {
+            Console.WriteLine("Prototype 2");
+        }
+        public override Prototype Clone()
+        {
+            return new ConcretePrototype2();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prototype ABCD = new ConcretePrototype1();
+            Prototype cloneABCD = ABCD.Clone();
+
+            Prototype EFGH = new ConcretePrototype2();
+            Prototype cloneEFGH = EFGH.Clone();
+        }
+    }
+}
+
